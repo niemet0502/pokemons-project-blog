8764418582f930aa71bf514082901f3ffd4dd092
niemet0502 <vincentmarius8@gmail.com>
2021-02-03 08:24:29 +0100

    main :: created Bulbizare component

---
+++ src/components/Bulbizarre/Bulbizarre.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Bulbizarre() {
+  return (
+    <div>
+      <h2>Bulbizarre</h2>
+      <img src="https://urlz.fr/eQ3n" alt=""/>
+    </div>
+  )
+}
+
+
